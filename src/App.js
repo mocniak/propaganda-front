255449d8c74b7e0df407fb8f2c17c9ca91d98152
Karol Mocniak <karol@mocniak.pl>
2017-12-04 22:33:08 +0100

    addable content fields

--- src/App.js
+++ src/App.js
@@ -14,10 +14,10 @@ class App extends React.Component {
         this.updateContent = this.updateContent.bind(this);
         this.updateTitle = this.updateTitle.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleAddField = this.handleAddField.bind(this);
     }
 
     componentDidMount() {
-        console.log('pre fetch');
         fetch('http://localhost:8000/admin/edit-article/c3079f45-f1b4-4104-ac38-48a897d84e8c')
             .then(console.log('fetched'))
             .then(response => response.json())
@@ -64,8 +64,33 @@ class App extends React.Component {
     }
 
     handleSubmit(event) {
-        alert('A name was submitted: ' + this.state.title);
+        let body = JSON.stringify({
+            title: this.state.title,
+            content: this.state.content
+        });
+        console.log(body);
+        fetch('http://localhost:8000/admin/submit-edit-article/c3079f45-f1b4-4104-ac38-48a897d84e8c', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: body
+        });
         event.preventDefault();
+    }
+
+    handleAddField(type) {
+        let content = this.state.content;
+        let newField = {
+            id: Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5),
+            type: type,
+            value: ''
+        };
+        content.push(newField);
+        this.setState({
+            content: content
+        });
     }
 
     render() {
@@ -101,10 +126,43 @@ class App extends React.Component {
                             </div>
                         )}
                     </div>
+                    <AddContentField addField={this.handleAddField}/>
+                    <hr/>
                     <input type="submit" value="Submit"/>
                 </form>
             )
         }
+    }
+}
+
+class AddContentField extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {value: 'text'};
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({value: event.target.value})
+    }
+
+    handleSubmit(event) {
+        this.props.addField(this.state.value);
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <div>
+                <select value={this.state.value} onChange={this.handleChange} name="newContent">
+                    <option value="text">Text</option>
+                    <option value="image">Image</option>
+                    <option value="video">Video</option>
+                </select>
+                <p onClick={this.handleSubmit}>Add content</p>
+            </div>
+        )
     }
 }
 
